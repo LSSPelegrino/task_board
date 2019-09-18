fa8bd76fe07e722a2878d7d35084e0a3e021a5ac
LSSPelegrino <lsspelegrino@gmail.com>
2019-09-18 16:12:04 -0300

    Added simple task viewer using Jinja2

--- app/routes.py
+++ app/routes.py
@@ -4,6 +4,7 @@ Describes routes for web requests
 
 # # 3rd Party Modules
 from flask import render_template
+import requests
 
 # # Internal Modules
 from app import app
@@ -14,48 +15,10 @@ def home():
     """
     Renders the application homepage
     """
-    tasks = [
-        {
-            'task_id': 1,
-            'title': 'Do it',
-            'description': 'Just do it',
-            'deadline': '2015-04-15T10:41:00.000Z',
-            'done': True,
-            'completed_at': '2019-09-17T01:05:36.447Z'},
-        {
-            'task_id': 2,
-            'title': 'Make your dreams come true',
-            'description': "Don't let your dreams be dreams",
-            'deadline': '2015-04-15T10:41:00.000Z',
-            'done': False,
-            'completed_at': None},
-        {
-            'task_id': 3,
-            'title': 'Rock the casbah',
-            'description': 'You have to let that raga drop',
-            'deadline': '1982-01-01T00:00:00.000Z',
-            'done': True,
-            'completed_at': '2019-09-15T04:16:42.595Z'},
-        {
-            'task_id': 4,
-            'title': 'Be yourself',
-            'description': 'All that I can do',
-            'deadline': '2005-02-02T00:00:00.000Z',
-            'done': False,
-            'completed_at': None},
-        {
-            'task_id': 5,
-            'title': 'first API call',
-            'description': 'First Time using PUT',
-            'deadline': '2019-09-16T17:12:00.000Z',
-            'done': False,
-            'completed_at': None},
-        {
-            'task_id': 6,
-            'title': 'Get up, stand up',
-            'description': "Don't give up the fight",
-            'deadline': '1981-05-11T05:00:00.000Z',
-            'done': False,
-            'completed_at': None}
-    ]
+    page = 1
+    per_page = 10
+    response = requests.get(
+        f"http://localhost:5000/api/tasks?page={page}&per_page={per_page}")
+    tasks = response.json()["items"]
+
     return render_template('home.html', tasks=tasks)
